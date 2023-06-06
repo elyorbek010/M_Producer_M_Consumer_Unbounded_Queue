44fa9e45c1e14d8e19a58dd09b6779b69b163799
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-06-06 15:53:26 +0500

    refactor: Make element array of a vector a separate entity

--- M_Producer_M_Consumer_Unbounded_Queue/vector.c
+++ M_Producer_M_Consumer_Unbounded_Queue/vector.c
@@ -14,33 +14,70 @@
         }                                              \
     }while(0)
 
-static inline size_t vector_next_index(const size_t index, const size_t capacity)
-{
-	return (index + 1) % (capacity + 1);
-}
-
-static inline size_t vector_prev_index(const size_t index, const size_t capacity)
-{
-	return index == 0 ? capacity : index - 1;
-}
-
-
 struct vector_t
 {
 	size_t capacity;
 	size_t begin;
 	size_t end;
-	void* element[];
+	void** element;
 };
+
+
+
+/*
+* FUNCTION DECLARATIONS
+*/
+
+
+
+vector_t* vector_create(const size_t capacity);
+vector_ret_t vector_destroy(vector_t* vector);
+
+vector_ret_t vector_push_begin(vector_t* vector, void* element);
+vector_ret_t vector_push_end(vector_t* vector, void* element);
+
+vector_ret_t vector_pop_begin(vector_t* vector, void* element);
+vector_ret_t vector_pop_end(vector_t* vector, void* element);
+
+vector_ret_t vector_peek_begin(const vector_t* vector, void** p_element);
+vector_ret_t vector_peek_end(const vector_t* vector, void** p_element);
+
+vector_ret_t vector_status(const vector_t* vector);
+
+vector_ret_t vector_get_capacity(const vector_t* vector, size_t* capacity);
+
+static vector_ret_t vector_enlarge(vector_t* vector);
+
+static inline size_t vector_next_index(const size_t index, const size_t capacity);
+static inline size_t vector_prev_index(const size_t index, const size_t capacity);
+
+
+
+/*
+* FUNCTION DEFINITIONS
+*/
+
+
 
 vector_t* vector_create(const size_t capacity)
 {
-	vector_t* vector = malloc(sizeof(*vector) + (capacity + 1) * sizeof(vector->element[0]));
+	vector_t* vector = malloc(sizeof(*vector));
+
 	if (vector == NULL)
 	{
 		debug_print("Not enough memory for capacity: %zd\n", capacity);
 		return NULL;
 	}
+
+	vector->element = malloc((capacity + 1) * sizeof(vector->element[0]));
+	
+	if (vector->element == NULL)
+	{
+		debug_print("Not enough memory for capacity: %zd\n", capacity);
+		free(vector);
+		return NULL;
+	}
+
 	vector->capacity = capacity;
 	vector->begin = vector->end = 0;
 
@@ -50,22 +87,9 @@ vector_t* vector_create(const size_t capacity)
 vector_ret_t vector_destroy(vector_t* vector)
 {
 	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
+	free(vector->element);
 	free(vector);
-	return VECTOR_SUCCESS;
-}
-
-vector_ret_t vector_push_end(vector_t* vector, void* element)
-{
-	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
-
-	vector->element[vector->end] = element;
-	vector->end = vector_next_index(vector->end, vector->capacity);
-
-	if (vector->begin == vector->end)
-	{
-		vector->begin = vector_next_index(vector->begin, vector->capacity);
-		return VECTOR_OVERFLOW;
-	}
+	
 	return VECTOR_SUCCESS;
 }
 
@@ -79,6 +103,21 @@ vector_ret_t vector_push_begin(vector_t* vector, void* element)
 	if (vector->begin == vector->end)
 	{
 		vector->end = vector_prev_index(vector->end, vector->capacity);
+		return VECTOR_OVERFLOW;
+	}
+	return VECTOR_SUCCESS;
+}
+
+vector_ret_t vector_push_end(vector_t* vector, void* element)
+{
+	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
+
+	vector->element[vector->end] = element;
+	vector->end = vector_next_index(vector->end, vector->capacity);
+
+	if (vector->begin == vector->end)
+	{
+		vector->begin = vector_next_index(vector->begin, vector->capacity);
 		return VECTOR_OVERFLOW;
 	}
 	return VECTOR_SUCCESS;
@@ -161,10 +200,19 @@ vector_ret_t vector_status(const vector_t* vector)
 }
 
 vector_ret_t vector_get_capacity(const vector_t* vector, size_t* capacity) {
-	if (vector == NULL) {
-		return VECTOR_FAILURE;
-	}
+	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
+	CHECK_AND_RETURN_IF_NOT_EXIST(capacity);
 
 	*capacity = vector->capacity;
 	return VECTOR_SUCCESS;
+}
+
+static inline size_t vector_next_index(const size_t index, const size_t capacity)
+{
+	return (index + 1) % (capacity + 1);
+}
+
+static inline size_t vector_prev_index(const size_t index, const size_t capacity)
+{
+	return index == 0 ? capacity : index - 1;
 }
