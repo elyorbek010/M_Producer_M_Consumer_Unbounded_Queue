d53331012997b475516725de440cdca9d0e4f5c2
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-06-06 15:54:06 +0500

    docs: Update function description comments

--- M_Producer_M_Consumer_Unbounded_Queue/vector.h
+++ M_Producer_M_Consumer_Unbounded_Queue/vector.h
@@ -18,62 +18,113 @@ typedef enum vector_ret_t
 
 /**
  * Creates a circular vector with `capacity` elements at most.
+ *
+ * RETURN VALUES:
+ * vector_t pointer
+ * NULL pointer -- when failed to allocate memory
+ * 
+ * [in] - capacity
  */
 vector_t* vector_create(size_t capacity);
 
 /**
- * Destroys the entire cvector with all its elements.
+ * Destroys the entire vector with all its elements.
+ *
+ * RETURN VALUES:
+ * VECTOR_SUCCESS -- vector is destroyed
+ * VECTOR_FAILURE -- vector is invalid
+ * 
+ * [in] - vector
  */
-vector_ret_t vector_destroy(vector_t* cvector);
+vector_ret_t vector_destroy(vector_t* vector);
 
 /**
- * Adds an element to the cvector.
+ * Adds an element to the vector.
  *
- * If the vector is full, VECTOR_OVERFLOW is returned
- * and `element` is written in place of the first element.
+ * RETURN VALUES:
+ * VECTOR_SUCCESS
+ * VECTOR_OVERFLOW -- vector is full and the first element is overwritten
+ * VECTOR_FAILURE -- vector or element is NULL pointer
+ * 
+ * [in] - vector, element
  */
-vector_ret_t vector_push_end(vector_t* cvector, void* element);
+vector_ret_t vector_push_begin(vector_t* vector, void* element);
 
 /**
- * Adds an element to the cvector.
+ * Adds an element to the vector.
  *
- * If the vector is full, VECTOR_OVERFLOW is returned
- * and `element` is written in place of the last element.
+ * RETURN VALUES:
+ * VECTOR_SUCCESS
+ * VECTOR_OVERFLOW -- vector is full and the last element is overwritten
+ * VECTOR_FAILURE -- vector or element is NULL pointer
+ *
+ * [in] - vector, element
  */
-vector_ret_t vector_push_begin(vector_t* cvector, void* element);
+vector_ret_t vector_push_end(vector_t* vector, void* element);
 
 /**
  * Removes an element from the vector.
+ *
+ * RETURN VALUES:
+ * VECTOR_SUCCESS
+ * VECTOR_UNDERFLOW -- vector is empty
+ * VECTOR_FAILURE -- vector or p_element are NULL pointers
+ * 
+ * [in] - vector
+ * [out] - p_element
  */
-vector_ret_t vector_pop_begin(vector_t* cvector, void** p_element);
+vector_ret_t vector_pop_begin(vector_t* vector, void** p_element);
 
 /**
  * Removes an element from the vector.
+ * RETURN VALUES:
+ * VECTOR_SUCCESS
+ * VECTOR_UNDERFLOW -- vector is empty
+ * VECTOR_FAILURE -- vector or p_element are NULL pointers
+ * 
+ * [in] - vector
+ * [out] - p_element
  */
-vector_ret_t vector_pop_end(vector_t* cvector, void** p_element);
+vector_ret_t vector_pop_end(vector_t* vector, void** p_element);
 
 /**
  * Peeks the first element at the front of the vector.
  */
-vector_ret_t vector_peek_begin(const vector_t* cvector, void** p_element);
+vector_ret_t vector_peek_begin(const vector_t* vector, void** p_element);
 
 /**
  * Peeks the last element at the end of the vector.
+ *
+ * RETURN VALUES:
+ * VECTOR_SUCCESS
+ * VECTOR_UNDERFLOW -- vector is empty
+ * VECTOR_FAILURE -- vector or p_element are NULL pointers
+ * 
+ * [in] - vector
+ * [out] - p_element
  */
-vector_ret_t vector_peek_end(const vector_t* cvector, void** p_element);
+vector_ret_t vector_peek_end(const vector_t* vector, void** p_element);
 
 /**
- * Returns VECTOR_FULL
- * Returns VECTOR_EMPTY
- * Returns VECTOR_NONEMPTY
+ * RETURN VALUES:
+ * VECTOR_FULL
+ * VECTOR_EMPTY
+ * VECTOR_NONEMPTY
+ * VECTOR_FAILURE -- vector is NULL pointer
+ * 
+ * [in] - vector
  */
-vector_ret_t vector_status(const vector_t* cvector);
+vector_ret_t vector_status(const vector_t* vector);
 
 /*
-* Returns VECTOR_FAILURE if vector does not exists
-* Returns VECTOR_SUCCESS otherwise
+* RETURN VALUES:
+* VECTOR_SUCCESS
+* VECTOR_FAILURE -- vector or capacity is NULL pointer
+* 
+* [in] - vector
+* [out] - capacity
 */
-vector_ret_t vector_get_capacity(const vector_t* cvector, size_t* capacity);
+vector_ret_t vector_get_capacity(const vector_t* vector, size_t* capacity);
 
 #endif // VECTOR_H
 
