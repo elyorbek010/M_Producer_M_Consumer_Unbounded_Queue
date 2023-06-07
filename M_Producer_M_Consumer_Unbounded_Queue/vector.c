c67bdeff39ccc47050c576166a07edd2b672ca46
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-06-07 23:17:44 +0500

    feat: Delete unnecessary functions, Add vector_enlarge(), Make Vector thread-safe

--- M_Producer_M_Consumer_Unbounded_Queue/vector.c
+++ M_Producer_M_Consumer_Unbounded_Queue/vector.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
+#include <pthread.h>
 
 #include "vector.h"
 #include "debug.h"
@@ -16,10 +16,13 @@
 
 struct vector_t
 {
-	size_t capacity;
-	size_t begin;
-	size_t end;
+	size_t capacity;	
+	size_t begin;		// begin index is inclusive
+	size_t end;			// end index is exclusive
 	void** element;
+
+	pthread_mutex_t mutex;
+	pthread_cond_t cond;
 };
 
 
@@ -33,16 +36,9 @@ struct vector_t
 vector_t* vector_create(const size_t capacity);
 vector_ret_t vector_destroy(vector_t* vector);
 
-vector_ret_t vector_push_end(vector_t* vector, void* element);
+vector_ret_t vector_push(vector_t* vector, void* element);
 
-vector_ret_t vector_pop_begin(vector_t* vector, void* element);
-
-vector_ret_t vector_peek_begin(const vector_t* vector, void** p_element);
-vector_ret_t vector_peek_end(const vector_t* vector, void** p_element);
-
-vector_ret_t vector_status(const vector_t* vector);
-
-vector_ret_t vector_get_capacity(const vector_t* vector, size_t* capacity);
+vector_ret_t vector_pop(vector_t* vector, void** element);
 
 static vector_ret_t vector_enlarge(vector_t* vector);
 
@@ -67,8 +63,8 @@ vector_t* vector_create(const size_t capacity)
 		return NULL;
 	}
 
-	vector->element = malloc((capacity + 1) * sizeof(vector->element[0]));
-	
+	vector->element = malloc((capacity + 1) * sizeof(vector->element[0])); // we add one more cell because end index is exclusive
+
 	if (vector->element == NULL)
 	{
 		debug_print("Not enough memory for capacity: %zd\n", capacity);
@@ -79,105 +75,130 @@ vector_t* vector_create(const size_t capacity)
 	vector->capacity = capacity;
 	vector->begin = vector->end = 0;
 
+	if (pthread_mutex_init(&vector->mutex, NULL) != 0 ||
+		pthread_cond_init(&vector->cond, NULL) != 0) {
+		vector_destroy(vector);
+		return NULL;
+	}
+
 	return vector;
 }
 
 vector_ret_t vector_destroy(vector_t* vector)
 {
 	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
+
+	pthread_mutex_destroy(&vector->mutex);
+	pthread_cond_destroy(&vector->cond);
+
 	free(vector->element);
 	free(vector);
 	
 	return VECTOR_SUCCESS;
 }
 
-vector_ret_t vector_push_end(vector_t* vector, void* element)
+vector_ret_t vector_push(vector_t* vector, void* element)
 {
 	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
+
+	if (pthread_mutex_lock(&vector->mutex) != 0)
+		return VECTOR_FAILURE;
+
+	if (vector_next_index(vector->end, vector->capacity) == vector->begin) {	// Vector full condition
+		if (vector_enlarge(vector) != VECTOR_SUCCESS) {
+			return VECTOR_FAILURE;
+		}
+	}
 
 	vector->element[vector->end] = element;
 	vector->end = vector_next_index(vector->end, vector->capacity);
 
-	if (vector->begin == vector->end)
-	{
-		vector->begin = vector_next_index(vector->begin, vector->capacity);
-		return VECTOR_OVERFLOW;
+	if (pthread_cond_signal(&vector->cond) != 0) {
+		pthread_mutex_unlock(&vector->mutex);
+		return VECTOR_FAILURE;
 	}
+
+	if (pthread_mutex_unlock(&vector->mutex) != 0)
+		return VECTOR_FAILURE;
+
 	return VECTOR_SUCCESS;
 }
 
-vector_ret_t vector_pop_begin(vector_t* vector, void** p_element)
+vector_ret_t vector_pop(vector_t* vector, void** p_element)
 {
 	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
 	CHECK_AND_RETURN_IF_NOT_EXIST(p_element);
 
-	if (vector->begin == vector->end)
+	if (pthread_mutex_lock(&vector->mutex) != 0)
+		return VECTOR_FAILURE;
+
+	while (vector->begin == vector->end)  // Vector empty condition
 	{
-		return VECTOR_UNDERFLOW;
+		if (pthread_cond_wait(&vector->cond, &vector->mutex) != 0) {
+			pthread_mutex_unlock(&vector->mutex);
+			return VECTOR_FAILURE;
+		}
 	}
 
 	*p_element = vector->element[vector->begin];
 	vector->begin = vector_next_index(vector->begin, vector->capacity);
+
+	if (pthread_mutex_unlock(&vector->mutex) != 0)
+		return VECTOR_FAILURE;
+
 	return VECTOR_SUCCESS;
 }
 
-vector_ret_t vector_peek_begin(const vector_t* vector, void** p_element)
-{
-	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
-	CHECK_AND_RETURN_IF_NOT_EXIST(p_element);
+static vector_ret_t vector_enlarge(vector_t* vector) {
+	void* new_location = NULL;
 
-	if (vector->begin == vector->end)
-	{
-		return VECTOR_UNDERFLOW;
-	}
-
-	*p_element = vector->element[vector->begin];
-	return VECTOR_SUCCESS;
-}
-
-vector_ret_t vector_peek_end(const vector_t* vector, void** p_element)
-{
-	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
-	CHECK_AND_RETURN_IF_NOT_EXIST(p_element);
-
-	if (vector->begin == vector->end)
-	{
-		return VECTOR_UNDERFLOW;
-	}
-
-	*p_element = vector->element[vector_prev_index(vector->end, vector->capacity)];
-	return VECTOR_SUCCESS;
-}
-
-vector_ret_t vector_status(const vector_t* vector)
-{
-	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
-
-	if (vector_next_index(vector->end, vector->capacity) == vector->begin)
-	{
-		return VECTOR_FULL;
-	}
-	else if (vector->begin == vector->end)
-	{
-		return VECTOR_EMPTY;
-	}
+	if (vector->capacity == 0)
+		new_location = malloc(2 * sizeof(vector->element[0])); // minimum space for one efficient cell
 	else
+		new_location = malloc((vector->capacity * 2 + 1) * sizeof(vector->element[0]));
+
+	if (new_location == NULL)
+		return VECTOR_FAILURE;
+
+	/*since the vector is cyclical, we must ensure that data does not partition incorrectly
+	* e.g. vector of size 3
+	* 
+	* e.g. |1|2|3|.| when enlarged becomes |1|2|3|.|.|.|.|.|
+	*       |     |                         |     |
+	*   begin(1)  |				       begin(1)   |
+	*           end(.)						    end(.)
+	* 
+	* Howerver, 
+	* 
+	*      |3|.|1|2| when enlarged might become  |3|.|1|2|.|.|.|.|
+	*         | |                                   | |
+	*         | begin(1)					        | begin(1) -- still overflow condition
+	*         end(.)						        end(.)
+	* 
+	* so, we need to copy it linearly and reset 'begin' and 'end' indexes
+	*/
+
+	if (vector->begin < vector->end) 
 	{
-		return VECTOR_NONEMPTY;
+		memcpy(new_location, vector->element, (vector->capacity + 1) * sizeof(vector->element[0]));
+	} 
+	else 
+	{
+		memcpy(new_location, vector->element + vector->begin, (vector->capacity + 1 - vector->begin) * sizeof(vector->element[0]));
+		memcpy(new_location, vector->element, vector->end * sizeof(vector->element[0]));
+
+		vector->begin = 0;
+		vector->end = (vector->capacity + 1 - vector->begin) + vector->end;
 	}
-}
 
-vector_ret_t vector_get_capacity(const vector_t* vector, size_t* capacity) {
-	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
-	CHECK_AND_RETURN_IF_NOT_EXIST(capacity);
+	vector->capacity = vector->capacity * 2 + 1;
 
-	*capacity = vector->capacity;
 	return VECTOR_SUCCESS;
 }
 
 static inline size_t vector_next_index(const size_t index, const size_t capacity)
 {
-	return (index + 1) % (capacity + 1);
+	return (index + 1) % (capacity + 1);	// increment index, maximum number of cells = capacity + 1 since end index is exclusive
 }
 
 static inline size_t vector_prev_index(const size_t index, const size_t capacity)
--- M_Producer_M_Consumer_Unbounded_Queue/vector.h
+++ M_Producer_M_Consumer_Unbounded_Queue/vector.h
@@ -8,16 +8,11 @@ typedef struct vector_t vector_t;
 typedef enum vector_ret_t
 {
 	VECTOR_SUCCESS = 0,
-	VECTOR_FAILURE = 1,
-	VECTOR_OVERFLOW,
-	VECTOR_UNDERFLOW,
-	VECTOR_EMPTY,
-	VECTOR_FULL,
-	VECTOR_NONEMPTY
+	VECTOR_FAILURE = 1
 } vector_ret_t;
 
 /**
- * Creates a circular vector with `capacity` elements at most.
+ * Create a circular vector with `capacity` elements at most.
  *
  * RETURN VALUES:
  * vector_t pointer
@@ -28,7 +23,7 @@ typedef enum vector_ret_t
 vector_t* vector_create(size_t capacity);
 
 /**
- * Destroys the entire vector with all its elements.
+ * Destroy the vector.
  *
  * RETURN VALUES:
  * VECTOR_SUCCESS -- vector is destroyed
@@ -39,68 +34,28 @@ vector_t* vector_create(size_t capacity);
 vector_ret_t vector_destroy(vector_t* vector);
 
 /**
- * Adds an element to the vector.
+ * Add an element to the vector.
  *
  * RETURN VALUES:
  * VECTOR_SUCCESS
- * VECTOR_OVERFLOW -- vector is full and the last element is overwritten
- * VECTOR_FAILURE -- vector or element is NULL pointer
+ * VECTOR_FAILURE -- vector or element is invalid, or malloc failed when enlarging vector
  *
  * [in] - vector, element
  */
-vector_ret_t vector_push_end(vector_t* vector, void* element);
+vector_ret_t vector_push(vector_t* vector, void* element);
 
 /**
- * Removes an element from the vector.
+ * Remove an element from the vector.
+ * Block the thread, when vector is empty, waiting for new data.
  *
  * RETURN VALUES:
  * VECTOR_SUCCESS
- * VECTOR_UNDERFLOW -- vector is empty
- * VECTOR_FAILURE -- vector or p_element are NULL pointers
+ * VECTOR_FAILURE -- vector or p_element is invalid
  * 
  * [in] - vector
  * [out] - p_element
  */
-vector_ret_t vector_pop_begin(vector_t* vector, void** p_element);
-
-/**
- * Peeks the first element at the front of the vector.
- */
-vector_ret_t vector_peek_begin(const vector_t* vector, void** p_element);
-
-/**
- * Peeks the last element at the end of the vector.
- *
- * RETURN VALUES:
- * VECTOR_SUCCESS
- * VECTOR_UNDERFLOW -- vector is empty
- * VECTOR_FAILURE -- vector or p_element are NULL pointers
- * 
- * [in] - vector
- * [out] - p_element
- */
-vector_ret_t vector_peek_end(const vector_t* vector, void** p_element);
-
-/**
- * RETURN VALUES:
- * VECTOR_FULL
- * VECTOR_EMPTY
- * VECTOR_NONEMPTY
- * VECTOR_FAILURE -- vector is NULL pointer
- * 
- * [in] - vector
- */
-vector_ret_t vector_status(const vector_t* vector);
-
-/*
-* RETURN VALUES:
-* VECTOR_SUCCESS
-* VECTOR_FAILURE -- vector or capacity is NULL pointer
-* 
-* [in] - vector
-* [out] - capacity
-*/
-vector_ret_t vector_get_capacity(const vector_t* vector, size_t* capacity);
+vector_ret_t vector_pop(vector_t* vector, void** p_element);
 
 #endif // VECTOR_H
 
