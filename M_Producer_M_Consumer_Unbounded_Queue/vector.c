f7fe4326ad7d456982807977e3b8eecd3d9f4ed8
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-06-08 19:44:24 +0500

    fix: vector_push() now unlocks mutex on receiving error
    style: Rename mutex and cond_var
    refactor: Separate the implementation of vector_push() and vector_pop()

--- M_Producer_M_Consumer_Unbounded_Queue/vector.c
+++ M_Producer_M_Consumer_Unbounded_Queue/vector.c
@@ -22,43 +22,37 @@ struct vector_t
 	size_t end;			// end index is exclusive
 	void** element;
 
-	pthread_mutex_t mutex;
-	pthread_cond_t cond;
+	pthread_mutex_t vector_guard;
+	pthread_cond_t avail;
 };
-
-
 
 /*
 * FUNCTION DECLARATIONS
 */
 
-
-
 vector_t* vector_create(const size_t capacity);
 vector_ret_t vector_destroy(vector_t* vector);
 
 vector_ret_t vector_push(vector_t* vector, void* element);
+static vector_ret_t vector_push_impl(vector_t* vector, void* element);
 
 vector_ret_t vector_pop(vector_t* vector, void** element);
+static vector_ret_t vector_pop_impl(vector_t* vector, void** element);
 
 static vector_ret_t vector_enlarge(vector_t* vector);
 
 static inline size_t vector_next_index(const size_t index, const size_t capacity);
 static inline size_t vector_prev_index(const size_t index, const size_t capacity);
 
-
-
 /*
 * FUNCTION DEFINITIONS
 */
-
-
 
 vector_t* vector_create(size_t capacity)
 {
 	vector_t* vector = malloc(sizeof(*vector));
 
-	if (vector == NULL)
+	if (vector == NULL)		// condition that malloc() failed
 	{
 		debug_print("Not enough memory for capacity: %zu\n", capacity);
 		return NULL;
@@ -66,7 +60,7 @@ vector_t* vector_create(size_t capacity)
 
 	vector->element = malloc((capacity + 1) * sizeof(vector->element[0])); // we add one more cell because end index is exclusive
 
-	if (vector->element == NULL)
+	if (vector->element == NULL)	// condition that malloc() failed
 	{
 		debug_print("Not enough memory for capacity: %zu\n", capacity);
 		free(vector);
@@ -76,9 +70,9 @@ vector_t* vector_create(size_t capacity)
 	vector->capacity = capacity;
 	vector->begin = vector->end = 0;
 
-	if (pthread_mutex_init(&vector->mutex, NULL) != 0 ||
-		pthread_cond_init(&vector->cond, NULL) != 0) {
-		debug_print("Could not initialize mutex or conditional variable\n");
+	if (pthread_mutex_init(&vector->vector_guard, NULL) != 0 ||
+		pthread_cond_init(&vector->avail, NULL) != 0) {
+		debug_print("Could not initialize vector_guard or conditional variable\n");
 		vector_destroy(vector);
 		return NULL;
 	}
@@ -92,8 +86,8 @@ vector_ret_t vector_destroy(vector_t* vector)
 {
 	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
 
-	pthread_mutex_destroy(&vector->mutex);
-	pthread_cond_destroy(&vector->cond);
+	pthread_mutex_destroy(&vector->vector_guard);
+	pthread_cond_destroy(&vector->avail);
 
 	free(vector->element);
 	free(vector);
@@ -105,25 +99,32 @@ vector_ret_t vector_push(vector_t* vector, void* element)
 {
 	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
 
-	if (pthread_mutex_lock(&vector->mutex) != 0)
+	if (pthread_mutex_lock(&vector->vector_guard) != 0)
 		return VECTOR_FAILURE;
 
+	if (vector_push_impl(vector, element) != VECTOR_SUCCESS) {
+		pthread_mutex_unlock(&vector->vector_guard);
+		return VECTOR_FAILURE;
+	}
+
+	if (pthread_mutex_unlock(&vector->vector_guard) != 0)
+		return VECTOR_FAILURE;
+
+	return VECTOR_SUCCESS;
+}
+
+static vector_ret_t vector_push_impl(vector_t* vector, void* element) {
 	if (vector_next_index(vector->end, vector->capacity) == vector->begin) {	// Vector full condition
-		if (vector_enlarge(vector) != VECTOR_SUCCESS) {
+		if (vector_enlarge(vector) != VECTOR_SUCCESS) {	
 			debug_print("Could not enlarge vector\n");
 			return VECTOR_FAILURE;
 		}
 	}
 
-	vector->element[vector->end] = element;
-	vector->end = vector_next_index(vector->end, vector->capacity);
+	vector->element[vector->end] = element;								// insert the element
+	vector->end = vector_next_index(vector->end, vector->capacity);		// update the 'end' index
 
-	if (pthread_cond_signal(&vector->cond) != 0) {
-		pthread_mutex_unlock(&vector->mutex);
-		return VECTOR_FAILURE;
-	}
-
-	if (pthread_mutex_unlock(&vector->mutex) != 0)
+	if (pthread_cond_signal(&vector->avail) != 0)		
 		return VECTOR_FAILURE;
 
 	return VECTOR_SUCCESS;
@@ -134,22 +135,26 @@ vector_ret_t vector_pop(vector_t* vector, void** p_element)
 	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
 	CHECK_AND_RETURN_IF_NOT_EXIST(p_element);
 
-	if (pthread_mutex_lock(&vector->mutex) != 0)
+	if (pthread_mutex_lock(&vector->vector_guard) != 0)
 		return VECTOR_FAILURE;
 
+	vector_pop_impl(vector, p_element);
+
+	if (pthread_mutex_unlock(&vector->vector_guard) != 0)
+		return VECTOR_FAILURE;
+
+	return VECTOR_SUCCESS;
+}
+
+static vector_ret_t vector_pop_impl(vector_t* vector, void** p_element) {
 	while (vector->begin == vector->end)  // Vector empty condition
 	{
-		if (pthread_cond_wait(&vector->cond, &vector->mutex) != 0) {
-			pthread_mutex_unlock(&vector->mutex);
+		if (pthread_cond_wait(&vector->avail, &vector->vector_guard) != 0)
 			return VECTOR_FAILURE;
-		}
 	}
 
 	*p_element = vector->element[vector->begin];
 	vector->begin = vector_next_index(vector->begin, vector->capacity);
-
-	if (pthread_mutex_unlock(&vector->mutex) != 0)
-		return VECTOR_FAILURE;
 
 	return VECTOR_SUCCESS;
 }
@@ -157,12 +162,13 @@ vector_ret_t vector_pop(vector_t* vector, void** p_element)
 static vector_ret_t vector_enlarge(vector_t* vector) {
 	void* new_location = NULL;
 
-	if (vector->capacity == 0)
+	if (vector->capacity == 0) {
 		new_location = malloc(2 * sizeof(vector->element[0])); // minimum space for one efficient cell
-	else
-		new_location = malloc((vector->capacity * 2 + 1) * sizeof(vector->element[0]));
+	} else {
+		new_location = malloc((vector->capacity * 2 + 1) * sizeof(vector->element[0])); // double capacity
+	}
 
-	if (new_location == NULL)
+	if (new_location == NULL)	// condition that malloc() failed
 		return VECTOR_FAILURE;
 
 	/*since the vector is cyclical, we must ensure that data does not partition incorrectly
