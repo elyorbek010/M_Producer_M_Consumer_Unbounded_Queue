5b340e99f4d087bc55cb15e946aeadca3275337a
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-06-09 09:01:18 +0500

    test: Refactor tests to remove copy/paste code

--- M_Producer_M_Consumer_Unbounded_Queue/test.cpp
+++ M_Producer_M_Consumer_Unbounded_Queue/test.cpp
@@ -15,77 +15,47 @@ void sleep(int sec) { Sleep(sec * 1000); } // Make function sleep(), compatible 
 #include <unistd.h>
 #endif
 
+void mpmc_simulate(size_t vector_size, size_t data_amount,
+	size_t producers_n, size_t consumers_n,
+	size_t producer_sleep, size_t consumer_sleep);
+
 /* Call functions with invalid(NULL) pointers*/
-TEST(SPSC, NULL_INPUT_TEST) {
+TEST(BASIC_OP, NULL_INPUT_TEST) {
 	vector_t* vector;
 
 	vector = vector_create(5);
 
 	int val = 0;
+	void* data_ptr = NULL;
+
+	EXPECT_EQ(vector_push(vector, nullptr), VECTOR_SUCCESS); // NULL is a valid value
+
+	EXPECT_EQ(vector_push(nullptr, &val), VECTOR_FAILURE);
+	EXPECT_EQ(vector_push(nullptr, nullptr), VECTOR_FAILURE);
+
+	EXPECT_EQ(vector_pop(vector, nullptr), VECTOR_FAILURE);
+	EXPECT_EQ(vector_pop(nullptr, &data_ptr), VECTOR_FAILURE);
+	EXPECT_EQ(vector_pop(nullptr, nullptr), VECTOR_FAILURE);
 
 	EXPECT_EQ(vector_destroy(nullptr), VECTOR_FAILURE);
-	EXPECT_EQ(vector_push(nullptr, &val), VECTOR_FAILURE);
-	EXPECT_EQ(vector_push(nullptr, &val), VECTOR_FAILURE);
-
-	EXPECT_EQ(vector_pop(vector, nullptr), VECTOR_FAILURE);
-	EXPECT_EQ(vector_pop(vector, nullptr), VECTOR_FAILURE);
 
 	vector_destroy(vector);
 }
 
-/*
-* Smoke test testing basic capabilities of the vector without corner cases
-* Create Vector
-* Push element
-* Pop element
-* Destroy vector
-*/
-TEST(SPSC, Smoke_Test_Vector)
+TEST(BASIC_OP, Push_Pop)
 {
 	vector_t* vector = nullptr;
 
-	int var1 = 10;
-	void* var2 = NULL;
+	int val = 0;
+	void* data_ptr = NULL;
 
 	vector = vector_create(5);
 
-	EXPECT_EQ(vector_push(vector, &var1), VECTOR_SUCCESS);
+	EXPECT_EQ(vector_push(vector, &val), VECTOR_SUCCESS);
 
-	EXPECT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
+	EXPECT_EQ(vector_pop(vector, &data_ptr), VECTOR_SUCCESS);
 
-	EXPECT_EQ(&var1, var2);
-
-	vector_destroy(vector);
-}
-
-/*Main: Push, Thread: Pop and vice versa*/
-TEST(SPSC, Thread_Smoke_Test_Vector)
-{
-	vector_t* vector = nullptr;
-	void* m_ptr = nullptr;
-
-	vector = vector_create(3);
-
-	std::thread t1([&vector]() {
-		void* t_ptr = nullptr;
-
-		sleep(1);
-
-		EXPECT_EQ(vector_pop(vector, &t_ptr), VECTOR_SUCCESS);
-		EXPECT_EQ(vector_push(vector, (void*)200), VECTOR_SUCCESS);
-
-		EXPECT_EQ((int)t_ptr, 100);	// verify the value
-	});
-
-	EXPECT_EQ(vector_push(vector, (void*)100), VECTOR_SUCCESS);
-
-	sleep(2);
-
-	EXPECT_EQ(vector_pop(vector, &m_ptr), VECTOR_SUCCESS);
-
-	EXPECT_EQ((int)m_ptr, 200); // verify the value
-
-	t1.join();
+	EXPECT_EQ(data_ptr, &val);
 
 	vector_destroy(vector);
 }
@@ -93,39 +63,44 @@ TEST(SPSC, Thread_Smoke_Test_Vector)
 /*
 * When queue overflows, it must allocate more space to fit in the new data
 */
-TEST(SPSC, FullQueue_Push)
+TEST(BASIC_OP, ZeroCapacity_Overflow)
 {
+	const size_t num_of_data = 10;
+
 	vector_t* vector = nullptr;
-	float var1 = 5;
-	void* var2;
+	void* data_ptr = nullptr;
 
-	vector = vector_create(1);
+	vector = vector_create(0);
 
-	// push values
-	ASSERT_EQ(vector_push(vector, &var1 + 0), VECTOR_SUCCESS); // 1/1 element
+	for (size_t data_n = 0; data_n < num_of_data; data_n++) {
+		EXPECT_EQ(vector_push(vector, (void*)data_n), VECTOR_SUCCESS);
+	}
 
-	EXPECT_EQ(vector_push(vector, &var1 + 1), VECTOR_SUCCESS); // enlarge 2/2 element
-	EXPECT_EQ(vector_push(vector, &var1 + 2), VECTOR_SUCCESS); // enlarge 3/4 element
+	for (size_t data_n = 0; data_n < num_of_data; data_n++) {
+		EXPECT_EQ(vector_pop(vector, &data_ptr), VECTOR_SUCCESS);
+		EXPECT_EQ((size_t)data_ptr, data_n);
+	}
 
-	ASSERT_EQ(vector_push(vector, &var1 + 3), VECTOR_SUCCESS); // 4/4 element
+	vector_destroy(vector);
+}
 
-	EXPECT_EQ(vector_push(vector, &var1 + 4), VECTOR_SUCCESS); // enlarge 5/8 element
+TEST(BASIC_OP, FullVector_Overflow)
+{
+	const size_t num_of_data = 10;
 
-	// pop values
-	ASSERT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
-	EXPECT_EQ(var2, &var1 + 0);
+	vector_t* vector = nullptr;
+	void* data_ptr = nullptr;
 
-	ASSERT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
-	EXPECT_EQ(var2, &var1 + 1);
+	vector = vector_create(4);
 
-	ASSERT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
-	EXPECT_EQ(var2, &var1 + 2);
+	for (size_t data_n = 0; data_n < num_of_data; data_n++) {
+		EXPECT_EQ(vector_push(vector, (void*)data_n), VECTOR_SUCCESS);
+	}
 
-	ASSERT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
-	EXPECT_EQ(var2, &var1 + 3);
-
-	ASSERT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
-	EXPECT_EQ(var2, &var1 + 4);
+	for (size_t data_n = 0; data_n < num_of_data; data_n++) {
+		EXPECT_EQ(vector_pop(vector, &data_ptr), VECTOR_SUCCESS);
+		EXPECT_EQ((size_t)data_ptr, data_n);
+	}
 
 	vector_destroy(vector);
 }
@@ -133,252 +108,267 @@ TEST(SPSC, FullQueue_Push)
 /*
 * Check that values can be inserted without blocking or waiting if enough space exists
 */
-TEST(SPSC, Circulation)
+TEST(BASIC_OP, Circulation)
 {
 	vector_t* vector = nullptr;
-	float var1 = 5;
-	void* var2;
+	void* data_ptr;
 
-	vector = vector_create(3);
+	vector = vector_create(5);
 
-	// push, vector elem 3/3
-	ASSERT_EQ(vector_push(vector, &var1 + 0), VECTOR_SUCCESS);
-	ASSERT_EQ(vector_push(vector, &var1 + 1), VECTOR_SUCCESS);
-	ASSERT_EQ(vector_push(vector, &var1 + 2), VECTOR_SUCCESS);
-
-	// pop, vector elem 1/3
-	ASSERT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
-	EXPECT_EQ(var2, &var1 + 0);
-	ASSERT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
-	EXPECT_EQ(var2, &var1 + 1);
-
-	// push, vector elem 3/3
-	EXPECT_EQ(vector_push(vector, &var1 + 3), VECTOR_SUCCESS);
-	EXPECT_EQ(vector_push(vector, &var1 + 4), VECTOR_SUCCESS);
-
-	// pop values
-	ASSERT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
-	EXPECT_EQ(var2, &var1 + 2);
-
-	ASSERT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
-	EXPECT_EQ(var2, &var1 + 3);
-
-	ASSERT_EQ(vector_pop(vector, &var2), VECTOR_SUCCESS);
-	EXPECT_EQ(var2, &var1 + 4);
-
-	vector_destroy(vector);
-}
-
-TEST(SPSC, EmptyQueue_Pop)
-{
-	vector_t* vector = nullptr;
-	void* ptr = nullptr;
-
-	vector = vector_create(3);
-
-	std::thread t1([&]() {
-		EXPECT_EQ(vector_pop(vector, &ptr), VECTOR_SUCCESS);
-		});
-
-	sleep(1);
-
-	ASSERT_EQ(ptr, nullptr); // since thread is blocked, nobody changed the value of ptr
-
-	EXPECT_EQ(vector_push(vector, (void*)100), VECTOR_SUCCESS);
-
-	t1.join();
-
-	EXPECT_EQ((int)ptr, 100); // after thread termination we can check the value
-
-	vector_destroy(vector);
-}
-
-TEST(SPMC, Concurrency)
-{
-	const size_t num_of_clients = 5;
-	size_t n = num_of_clients * 5;
-	int result[num_of_clients] = { 0 };
-
-	vector_t* vector = vector_create(n);
-
-	std::vector<std::thread> clients;
-	for (size_t i = 0; i < num_of_clients; i++) {							// create num_of_clients amount of threads
-		{
-			clients.push_back(std::thread([i, vector, &result]() {			// save thread id in clients vector to join them later
-				
-				void* data = nullptr;
-
-				for (size_t j = 0; j < 5; j++) {							// vector_pop 5 times in each thread
-					EXPECT_EQ(vector_pop(vector, &data), VECTOR_SUCCESS);
-					result[i] += (int)data;									// save data in shared pool
-				}
-
-			}));
-		}
-	}
-
-	for (size_t i = 0; i < n; i++) {									// push num_of_clients * 5 amount of data
+	// push, vector elem 5/5
+	for (size_t i = 0; i < 5; i++) {
 		ASSERT_EQ(vector_push(vector, (void*)i), VECTOR_SUCCESS);
-		if (rand() % 2 == 0)											// sleep sometimes
-			sleep(1);
+	}
+	
+	// pop, vector elem 0/5
+	for (size_t i = 0; i < 5; i++) {
+		ASSERT_EQ(vector_pop(vector, &data_ptr), VECTOR_SUCCESS);
+		ASSERT_EQ((size_t)data_ptr, i);
 	}
 
-	std::for_each(clients.begin(), clients.end(), [](std::thread& t)	// join all threads
-		{
-			t.join();
-		});
-
-	vector_destroy(vector);
-
-	int sum_popped = 0;
-	sum_popped = std::accumulate(result, result + num_of_clients, 0);	// calculate the sum of all popped values
-
-	EXPECT_EQ(sum_popped, (n - 1) * n / 2);	 // check if the sum of all popped data is equal to the sum of all pushed data
-}
-
-TEST(MPSC, VectorOverflow)
-{
-	const size_t num_of_clients = 5;
-	size_t n = num_of_clients * 5;
-	int result[num_of_clients] = { 0 };
-
-	vector_t* vector = vector_create(n / 2);	// vector will enlarge to fit in the new data
-
-	std::vector<std::thread> clients;
-	for (size_t i = 0; i < num_of_clients; i++) {							// create num_of_clients amount of threads
-		{
-			clients.push_back(std::thread([i, vector, &result]() {			// save thread id in clients vector to join them later
-
-				for (size_t j = 0; j < 5; j++) {							// vector_push 5 times in each thread
-					EXPECT_EQ(vector_push(vector, (void *)(i * j)), VECTOR_SUCCESS);
-					result[i] += (int)(i * j);								// save data in shared pool
-					if (rand() % 2 == 0)										
-						sleep(1);											// sleep sometimes
-				}
-
-			}));
-		}
+	// push, vector elem 5/5
+	for (size_t i = 6; i < 10; i++) {
+		ASSERT_EQ(vector_push(vector, (void*)i), VECTOR_SUCCESS);
 	}
 
-	std::for_each(clients.begin(), clients.end(), [](std::thread& t)	// join all threads
-		{
-			t.join();
-		});
-
-	void* data = nullptr;
-	int sum_popped = 0;
-	for (size_t i = 0; i < n; i++) {									// pop num_of_clients * 5 amount of data
-		ASSERT_EQ(vector_pop(vector, &data), VECTOR_SUCCESS);
-		sum_popped += (int)data;
+	// pop, vector elem 0/5
+	for (size_t i = 6; i < 10; i++) {
+		ASSERT_EQ(vector_pop(vector, &data_ptr), VECTOR_SUCCESS);
+		ASSERT_EQ((size_t)data_ptr, i);
 	}
 
 	vector_destroy(vector);
-
-	int sum_pushed = std::accumulate(result, result + num_of_clients, 0); // calculate the sum of all popped values
-
-	EXPECT_EQ(sum_popped, sum_pushed);	 // check if the sum of all popped data is equal to the sum of all pushed data
 }
 
-TEST(MPSC, Concurrency)
+TEST(SPSC, Push_Pop)
 {
-	const size_t num_of_clients = 5;
-	size_t n = num_of_clients * 5;
-	int result[num_of_clients] = { 0 };
+	size_t vector_size = 1;
+	size_t data_amount = 1;
+	size_t producers_n = 1;
+	size_t consumers_n = 1;
+	size_t producer_sleep = 0;
+	size_t consumer_sleep = 1;
 
-	vector_t* vector = vector_create(n / 2);	// vector will enlarge to fit in the new data
-
-	std::vector<std::thread> clients;
-	for (size_t i = 0; i < num_of_clients; i++) {							// create num_of_clients amount of threads
-		{
-			clients.push_back(std::thread([i, vector, &result]() {			// save thread id in clients vector to join them later
-
-				for (size_t j = 0; j < 5; j++) {							// vector_push 5 times in each thread
-					EXPECT_EQ(vector_push(vector, (void*)(i * j)), VECTOR_SUCCESS);
-					result[i] += (int)(i * j);								// save data in shared pool
-					if (rand() % 2 == 0)
-						sleep(1);											// sleep sometimes
-				}
-
-				}));
-		}
-	}
-
-	void* data = nullptr;
-	int sum_popped = 0;
-	for (size_t i = 0; i < n; i++) {									// pop num_of_clients * 5 amount of data
-		ASSERT_EQ(vector_pop(vector, &data), VECTOR_SUCCESS);
-		sum_popped += (int)data;
-	}
-
-	std::for_each(clients.begin(), clients.end(), [](std::thread& t)	// join all threads
-		{
-			t.join();
-		});
-
-	vector_destroy(vector);
-
-	int sum_pushed = std::accumulate(result, result + num_of_clients, 0); // calculate the sum of all popped values
-
-	EXPECT_EQ(sum_popped, sum_pushed);	 // check if the sum of all popped data is equal to the sum of all pushed data
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
 }
 
-TEST(MPMC, Concurrency)
+TEST(SPSC, Pop_Block_Push)
 {
-	const size_t num_of_threads = 5;
-	size_t n = num_of_threads * 5;
-	int result_clients[num_of_threads] = { 0 };
-	int result_producers[num_of_threads] = { 0 };
+	size_t vector_size = 1;
+	size_t data_amount = 1;
+	size_t producers_n = 1;
+	size_t consumers_n = 1;
+	size_t producer_sleep = 1;
+	size_t consumer_sleep = 0;
 
-	vector_t* vector = vector_create(n / 2);	// vector will enlarge to fit in the new data
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
 
-	std::vector<std::thread> clients;
-	for (size_t i = 0; i < num_of_threads; i++) {									// create num_of_clients amount of threads
-		{
-			clients.push_back(std::thread([i, vector, &result_clients]() {			// save thread id in clients vector to join them later
+TEST(SPSC, FullVector_Overflow)
+{
+	size_t vector_size = 10;
+	size_t data_amount = 50;
+	size_t producers_n = 1;
+	size_t consumers_n = 1;
+	size_t producer_sleep = 0;
+	size_t consumer_sleep = 1;
 
-				void* data = nullptr;
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
 
-				for (size_t j = 0; j < 5; j++) {									// vector_pop 5 times in each thread
-					EXPECT_EQ(vector_pop(vector, &data), VECTOR_SUCCESS);
-					result_clients[i] += (int)data;								// save data in shared pool
-					if (rand() % 2 == 0)
-						sleep(1);													// sleep sometimes
-				}
+TEST(SPMC, Push_Pop)
+{
+	size_t vector_size = 20;
+	size_t data_amount = 20;
+	size_t producers_n = 1;
+	size_t consumers_n = 5;
+	size_t producer_sleep = 0;
+	size_t consumer_sleep = 1;
 
-			}));
-		}
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
+
+TEST(SPMC, Pop_Block_Push)
+{
+	size_t vector_size = 20;
+	size_t data_amount = 20;
+	size_t producers_n = 1;
+	size_t consumers_n = 5;
+	size_t producer_sleep = 1;
+	size_t consumer_sleep = 0;
+
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
+
+TEST(SPMC, FullVector_Overflow)
+{
+	size_t vector_size = 10;
+	size_t data_amount = 50;
+	size_t producers_n = 1;
+	size_t consumers_n = 5;
+	size_t producer_sleep = 0;
+	size_t consumer_sleep = 1;
+
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
+
+TEST(MPSC, Push_Pop)
+{
+	size_t vector_size = 20;
+	size_t data_amount = 20;
+	size_t producers_n = 5;
+	size_t consumers_n = 1;
+	size_t producer_sleep = 0;
+	size_t consumer_sleep = 1;
+
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
+
+TEST(MPSC, Pop_Block_Push)
+{
+	size_t vector_size = 20;
+	size_t data_amount = 20;
+	size_t producers_n = 5;
+	size_t consumers_n = 1;
+	size_t producer_sleep = 1;
+	size_t consumer_sleep = 0;
+
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
+
+TEST(MPSC, FullVector_Overflow)
+{
+	size_t vector_size = 10;
+	size_t data_amount = 50;
+	size_t producers_n = 5;
+	size_t consumers_n = 1;
+	size_t producer_sleep = 0;
+	size_t consumer_sleep = 1;
+
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
+
+TEST(MPMC, Push_Pop)
+{
+	size_t vector_size = 20;
+	size_t data_amount = 20;
+	size_t producers_n = 5;
+	size_t consumers_n = 5;
+	size_t producer_sleep = 0;
+	size_t consumer_sleep = 1;
+
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
+
+TEST(MPMC, Pop_Block_Push)
+{
+	size_t vector_size = 20;
+	size_t data_amount = 20;
+	size_t producers_n = 5;
+	size_t consumers_n = 5;
+	size_t producer_sleep = 1;
+	size_t consumer_sleep = 0;
+
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
+
+TEST(MPMC, FullVector_Overflow)
+{
+	size_t vector_size = 10;
+	size_t data_amount = 20;
+	size_t producers_n = 5;
+	size_t consumers_n = 5;
+	size_t producer_sleep = 1;
+	size_t consumer_sleep = 0;
+
+	mpmc_simulate(vector_size, data_amount, producers_n, consumers_n, producer_sleep, consumer_sleep);
+}
+
+// Recursive function to return gcd of a and b
+long long gcd(long long int a, long long int b)
+{
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+
+// Function to return LCM of two numbers
+long long lcm(int a, int b)
+{
+	return (a / gcd(a, b)) * b;
+}
+
+int my_accumulate(int* result, size_t n) {
+	return std::accumulate(result, result + n, 0);
+}
+
+void mpmc_simulate(size_t vector_size, size_t data_amount,
+	size_t producers_n, size_t consumers_n,
+	size_t producer_sleep, size_t consumer_sleep)
+{
+	// we need to be sure that data_amount is divisible by both producers_n and consumers_n
+	long long alignment = lcm(producers_n, consumers_n);
+	if (data_amount % alignment != 0) {
+		data_amount += (alignment - data_amount % alignment);
 	}
 
+	vector_t* vector = vector_create(vector_size);
+
+	// consumers_result[i] -- the data popped by consumer 'i'
+	int* consumers_result = new int[consumers_n];
+	memset(consumers_result, 0, consumers_n * sizeof(int));
+
+	// producers_result[i] -- the data pushed by producer 'i'
+	int* producers_result = new int[producers_n];
+	memset(producers_result, 0, producers_n * sizeof(int));
+
+	// Vector contains thread IDs
 	std::vector<std::thread> producers;
-	for (size_t i = 0; i < num_of_threads; i++) {									// create num_of_clients amount of threads
-		{
-			producers.push_back(std::thread([i, vector, &result_producers]() {		// save thread id in producers vector to join them later
+	std::vector<std::thread> consumers;
 
-				for (size_t j = 0; j < 5; j++) {									// vector_push 5 times in each thread
-					EXPECT_EQ(vector_push(vector, (void*)(i * j)), VECTOR_SUCCESS);
-					result_producers[i] += (int)(i * j);							// save data in shared pool
-					if (rand() % 2 == 0)
-						sleep(1);													// sleep sometimes
-				}
+	// create producers_n amount of producer threads
+	for (size_t thread_n = 0; thread_n < producers_n; thread_n++) {
+		producers.push_back(std::thread([=, &producers_result]() {
 
-			}));
-		}
+			sleep(producer_sleep);
+
+			for (size_t iter = 0; iter < (unsigned int)((double)data_amount / (double)producers_n); iter++) {
+				EXPECT_EQ(vector_push(vector, (void*)(thread_n * iter)), VECTOR_SUCCESS);
+				producers_result[thread_n] += (int)(thread_n * iter);
+			}
+
+			})
+		);
 	}
 
-	std::for_each(clients.begin(), clients.end(), [](std::thread& t)		// join producers
-		{
-			t.join();
-		});
+	// create consumers_n amount of consumer threads
+	for (size_t thread_n = 0; thread_n < consumers_n; thread_n++) {
+		consumers.push_back(std::thread([=, &consumers_result]() {
+		
+			void* data_ptr = nullptr;
 
-	std::for_each(producers.begin(), producers.end(), [](std::thread& t)	// join clients
-		{
-			t.join();
-		});
+			sleep(consumer_sleep);
+
+			for (size_t iter = 0; iter < (unsigned int)((double)data_amount / (double)consumers_n); iter++) {
+				EXPECT_EQ(vector_pop(vector, &data_ptr), VECTOR_SUCCESS);
+				consumers_result[thread_n] += (int)data_ptr;
+			}
+
+			})
+		);
+	}
+
+	// Join producers
+	std::for_each(producers.begin(), producers.end(), [](std::thread& t1) { t1.join(); });
+
+	// Join consumers
+	std::for_each(consumers.begin(), consumers.end(), [](std::thread& t2) { t2.join(); });
 
 	vector_destroy(vector);
 
-	int sum_producers = std::accumulate(result_producers, result_producers + num_of_threads, 0);// calculate the sum of all pushed values
-	int sum_clients = std::accumulate(result_clients, result_clients + num_of_threads, 0);		// calculate the sum of all popped values
+	// Compare the sum of pushed and popped data
+	EXPECT_EQ(my_accumulate(producers_result, producers_n), my_accumulate(consumers_result, consumers_n));
 
-	EXPECT_EQ(sum_producers, sum_clients);	 // check if the sum of all popped data is equal to the sum of all pushed data
+	delete[] producers_result;
+	delete[] consumers_result;
 }
