fc5cd78e6a7e094fa6dc2494e26535723aea4029
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-06-23 13:28:44 +0000

    benchmark: make threads execute at the same time

--- mpmc/benchmark/mpmc_benchmark.cpp
+++ mpmc/benchmark/mpmc_benchmark.cpp
@@ -12,29 +12,33 @@ extern "C"
 
 #define LOG_ENABLED 0
 
+pthread_barrier_t thread_ready;
+
 void spsc_simulate(size_t vector_size, size_t data_amount)
 {
   vector_t *vector = vector_create(vector_size);
 
   auto producer = std::thread([=]()
                               {
+                                pthread_barrier_wait(&thread_ready);
                                 vector_ret_t ret = VECTOR_SUCCESS;
                                 size_t iter = 0;
 #if LOG_ENABLED == 1
                                 std::string filename = "producer_logs_" + std::to_string(data_amount) + ".txt";
-
-                                std::ofstream producer_logs;
-
-                                producer_logs.open(filename, std::ios::out | std::ios::trunc);
+                                std::ofstream producer_logs(filename, std::ios::out | std::ios::trunc);
                                 if (!producer_logs.is_open())
                                 {
-                                  std::cout << "error opening" << filename << std::endl;
+                                  std::cout << "error opening log file" << filename << std::endl;
+                                  abort();
                                 }
 #endif
                                 while (iter < data_amount)
                                 {
                                   ret = vector_push(vector, (void *)iter);
-
+#if LOG_ENABLED
+                                  auto time = std::chrono::system_clock::now().time_since_epoch() /
+                                              std::chrono::nanoseconds(1);
+#endif
                                   if (ret == VECTOR_FAILURE)
                                   {
                                     abort();
@@ -45,8 +49,7 @@ void spsc_simulate(size_t vector_size, size_t data_amount)
                                     producer_logs << "PUSHED "
                                                   << iter
                                                   << " "
-                                                  << std::chrono::system_clock::now().time_since_epoch() /
-                                                         std::chrono::nanoseconds(1)
+                                                  << time
                                                   << "\n";
 #endif
                                     iter++;
@@ -59,32 +62,42 @@ void spsc_simulate(size_t vector_size, size_t data_amount)
 
   auto consumer = std::thread([=]()
                               {
+                                pthread_barrier_wait(&thread_ready);
                                 vector_ret_t ret = VECTOR_SUCCESS;
                                 void *data_ptr = nullptr;
                                 size_t iter = 0;
 #if LOG_ENABLED == 1
                                 std::string filename = "consumer_logs_" + std::to_string(data_amount) + ".txt";
-
-                                std::ofstream consumer_logs;
-                                consumer_logs.open(filename, std::ios::out | std::ios::trunc);
+                                std::ofstream consumer_logs(filename, std::ios::out | std::ios::trunc);
+                                if (!consumer_logs.is_open())
+                                {
+                                  std::cout << "error opening log file" << filename << std::endl;
+                                  abort();
+                                }
 #endif
                                 while (iter < data_amount)
                                 {
                                   ret = vector_pop(vector, &data_ptr);
 #if LOG_ENABLED == 1
-                                  consumer_logs << "POPPED "
-                                                << " "
-                                                << (size_t)data_ptr
-                                                << std::chrono::system_clock::now().time_since_epoch() /
-                                                       std::chrono::nanoseconds(1)
-                                                << "\n";
+                                  auto time = std::chrono::system_clock::now().time_since_epoch() /
+                                              std::chrono::nanoseconds(1);
 #endif
                                   if (ret == VECTOR_FAILURE)
                                   {
                                     abort();
                                   }
-                                  else if (ret == VECTOR_SUCCESS)
+                                  else
+                                  {
+#if LOG_ENABLED == 1
+                                    consumer_logs << "POPPED "
+                                                  << iter 
+                                                  << " "
+                                                  << (size_t)data_ptr
+                                                  << time
+                                                  << "\n";
+#endif
                                     iter++;
+                                  }
                                 }
 #if LOG_ENABLED == 1
                                 consumer_logs.close();
@@ -100,6 +113,7 @@ void spsc_simulate(size_t vector_size, size_t data_amount)
 
 static void Bench_spsc_simulate(benchmark::State &state)
 {
+  pthread_barrier_init(&thread_ready, NULL, 2);
   for (auto _ : state)
   {
     spsc_simulate(1000, state.range(0));
